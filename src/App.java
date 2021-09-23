d9d52d3b8353fda581911f060ea61f7bbb7fbe89
dongho902 <dongho31089@gmail.com>
2021-09-23 09:02:28 +0700

    lệnh nhập kho đầu tiên

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       System.out.println("hello mi bạn nhá");
+        System.out.println("Hello, World!");
+    }
+
+}
+
+
