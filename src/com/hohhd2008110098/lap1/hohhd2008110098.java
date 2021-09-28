756c6bfa6a849f4080c820d1fa7bda8a0fd757be
dongho902 <dongho31089@gmail.com>
2021-09-29 00:45:18 +0700

    bài tập

---
+++ src/com/hohhd2008110098/lap1/hohhd2008110098.java
@@ -0,0 +1,14 @@
+package com.hohhd2008110098.lap1;
+import java.util.Scanner;
+
+public class hohhd2008110098 {
+    public static void main(String[] args) {
+        Scanner bien = new Scanner(System.in);
+        System.out.println("Ho Hoang Dong Ho");
+        String hoten = bien.nextLine();
+
+        System.out.println("diem TB");
+        double diemTB = bien.nextDouble();
+    }
+    
+}
